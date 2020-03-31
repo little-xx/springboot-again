4763a4a16f7cb489b119f3682d2570578dd1de22
email <littlexx-PC>
2020-04-01 00:20:57 +0800

    learn yaml

--- src/test/java/com/littlexx/springbootagain/SpringbootagainApplicationTests.java
+++ src/test/java/com/littlexx/springbootagain/SpringbootagainApplicationTests.java
@@ -1,13 +1,19 @@
 package com.littlexx.springbootagain;
 
+import com.littlexx.springbootagain.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootagainApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
