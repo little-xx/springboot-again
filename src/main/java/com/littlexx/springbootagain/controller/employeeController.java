8f8385b47d6f6c2483ea44e321931fca7ba69a36
littlexx <997864406@qq.com>
2020-04-09 23:13:59 +0800

    learn restful CRUD

--- src/main/java/com/littlexx/springbootagain/controller/employeeController.java
+++ src/main/java/com/littlexx/springbootagain/controller/employeeController.java
@@ -1,11 +1,16 @@
 package com.littlexx.springbootagain.controller;
 
+import com.littlexx.springbootagain.dao.DepartmentDao;
 import com.littlexx.springbootagain.dao.EmployeeDao;
+import com.littlexx.springbootagain.entities.Department;
 import com.littlexx.springbootagain.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -15,10 +20,41 @@ public class employeeController {
     @Autowired
     EmployeeDao employeeDao;
 
-    @RequestMapping("/emps")
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditEmp(@PathVariable("id") Integer id, Model model) {
+        Employee emp = employeeDao.get(id);
+        model.addAttribute("emp", emp);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String editEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
